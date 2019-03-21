3102a8a3c385509fc75cecb9ae921c5eca357159
Mathias Aarseth Pedersen <Mathias.Pedersen@nordicsemi.no>
2019-03-21 17:48:37 +0100

    WIP

--- lib/components/AppMainView.jsx
+++ lib/components/AppMainView.jsx
@@ -35,60 +35,90 @@
  */
 
 import React from 'react';
-import { Button, FormGroup, ControlLabel, FormControl, InputGroup } from 'react-bootstrap';
+import { Button, FormGroup, ControlLabel, FormControl, InputGroup, Dropdown, SplitButton } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import { logger } from 'nrfconnect/core';
-import { DTM_FREQUENCY } from 'nrf-dtm-js';
+import { DTM_FREQUENCY, DTM_PKT } from 'nrf-dtm-js';
 
 class AppMainView extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            frequency: 2402
+            length: 0,
+            bitPattern: DTM_PKT.PAYLOAD_PRBS9,
+
+            useSingleChannel: true,
+            singleChannelNum: 0,
+            multiChannelLowNum: 0,
+            multiChannelHighNum: 39,
+            multiChannelMidNum: 19,
+
         };
     }
 
-    onFrequencyChange(event) {
-        this.setState({ frequency: event.target.value });
+    onSingleChannelChange(event) {
+        this.setState({ singleChannelNum: event.target.value });
     };
+    onLengthChange(event) {
+        this.setState({ length: event.target.value });
+    };
+    onPayloadModelChanged(event){
+        this.setState({ bitPattern: event.target.value });
+    }
 
     render() {
         console.log(this.props);
         const { dtm } = this.props;
         const testSetup = async () => {
             logger.info('Test setup');
-            const cmd = dtm.createSetupCMD();
+            let response  = await dtm.setupReset();
+            logger.info('Response: ', response);
+            response  = await dtm.setupLength(this.state.length);
+            logger.info('Response: ', response);
+            /*const cmd = dtm.createSetupCMD();
             logger.info('Command: ', Buffer(cmd));
             const response = await dtm.sendCMD(cmd);
-            logger.info('Response: ', response);
+            logger.info('Response: ', response);*/
         };
 
         const testEnd = async () => {
             logger.info('Test end');
-            const cmd = dtm.createEndCMD();
+            const response = dtm.endTest();
+            /*const cmd = dtm.createEndCMD();
             logger.info('Command: ', Buffer(cmd));
             const response = await dtm.sendCMD(cmd);
             logger.info('Response: ', response);
             const count = response.readUIntBE(0, 2) - Buffer.from([0x80, 0x00]).readUIntBE(0, 2)
             console.log(count);
-            logger.info(`Packet count: ${count}`);
+            logger.info(`Packet count: ${count}`);*/
         };
 
         const transmitterTest = async () => {
             logger.info('Transmitter test');
-            const cmd = dtm.createTransmitterCMD(DTM_FREQUENCY(this.state.frequency));
+            const response  = dtm.transmitterTest(
+                parseInt(this.state.bitPattern, 10),
+                parseInt(this.state.length, 10),
+                {useSingleChannel: this.state.useSingleChannel,
+                singleChannelNum: parseInt(this.state.singleChannelNum, 10),
+                multiChannelLowNum: parseInt(this.state.multiChannelLowNum, 10),
+                multiChannelHighNum: parseInt(this.state.multiChannelHighNum, 10),
+                multiChannelMidNum: parseInt(this.state.multiChannelMidNum, 10)}
+            );
+            logger.info('Response: ', response);
+            /*const cmd = dtm.createTransmitterCMD(DTM_FREQUENCY(this.state.frequency));
             logger.info('Command: ', Buffer(cmd));
             const response = await dtm.sendCMD(cmd);
-            logger.info('Response: ', response);
+            logger.info('Response: ', response);*/
         };
 
         const receiverTest = async () => {
-            logger.info('Recever test');
+            /*logger.info('Recever test');
             const cmd = dtm.createReceiverCMD(DTM_FREQUENCY(this.state.frequency));
             logger.info('Command: ', Buffer(cmd));
             const response = await dtm.sendCMD(cmd);
-            logger.info('Response: ', response);
+            logger.info('Response: ', response);*/
         };
+
 
         return (
             <div className="app-main-view">
@@ -104,11 +134,26 @@ class AppMainView extends React.Component {
                 </Button>
 
                 <FormGroup>
-                    <ControlLabel>Frequency (2402 - 2480):</ControlLabel>
+                    <ControlLabel>Channel (0 - 39):</ControlLabel>
                     <InputGroup>
-                        <FormControl value={ this.state.frequency } onChange={ this.onFrequencyChange.bind(this) } />
+                        <FormControl value={ this.state.singleChannelNum } onChange={ this.onSingleChannelChange.bind(this) } />
                     </InputGroup>
                 </FormGroup>
+
+                <FormGroup>
+                    <ControlLabel>Payload type:</ControlLabel>
+                    <InputGroup>
+                        <FormControl value={ this.state.bitPattern } onChange={ this.onPayloadModelChanged.bind(this) } />
+                    </InputGroup>
+                </FormGroup>
+
+                <FormGroup>
+                    <ControlLabel>Payload length:</ControlLabel>
+                    <InputGroup>
+                        <FormControl value={ this.state.length } onChange={ this.onLengthChange.bind(this) } />
+                    </InputGroup>
+                </FormGroup>
+
 
                 <Button
                     onClick = { transmitterTest }
@@ -120,6 +165,7 @@ class AppMainView extends React.Component {
                 >
                     Receiver test
                 </Button>
+
             </div>
         );
     }

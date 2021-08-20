f35d738f875a41dd817cb8a1fbe5cc43e85aa721
Pascal Pickel <Pascal.Pickel@nordicsemi.no>
2021-08-20 15:17:48 +0200

    test: use StateSelector component from shared
    Signed-off-by: Pascal Pickel <Pascal.Pickel@nordicsemi.no>

--- src/SidePanel/ToggleChannelModeView.tsx
+++ src/SidePanel/ToggleChannelModeView.tsx
@@ -36,8 +36,8 @@
 
 // eslint-disable-next-line import/no-unresolved
 import React from 'react';
-import { Button, ButtonGroup } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
+import { StateSelector } from 'pc-nrfconnect-shared';
 import PropTypes from 'prop-types';
 
 import channelModeChanged from '../actions/settingsActions';
@@ -48,27 +48,18 @@ interface ToggleChannelModeViewProps {
 }
 
 const ToggleChannelModeView = ({ isRunning }: ToggleChannelModeViewProps) => {
-    const selected = useSelector(getChannelMode);
-
     const dispatch = useDispatch();
 
-    const selectionButton = (type: string, text: string) => (
-        <Button
-            // @ts-ignore -- Doesn't seem to be an easy way to use custom variants with TS
-            variant={selected === type ? 'set' : 'unset'}
-            onClick={() => dispatch(channelModeChanged(type))}
-            active={selected === type}
-            disabled={isRunning}
-        >
-            {text}
-        </Button>
-    );
-
     return (
-        <ButtonGroup className="w-100 d-flex flex-row channel-selection">
-            {selectionButton(DTM_CHANNEL_MODE.single, 'Single')}
-            {selectionButton(DTM_CHANNEL_MODE.sweep, 'Sweep')}
-        </ButtonGroup>
+        <StateSelector
+            items={['Single', 'Sweep']}
+            onSelect={(type: number) =>
+                dispatch(
+                    channelModeChanged(Object.values(DTM_CHANNEL_MODE)[type])
+                )
+            }
+            disabled={isRunning}
+        />
     );
 };
 

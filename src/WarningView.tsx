fcb5e71c258d24f529642684088a836af18fcf13
Jonas Fritzke Emdal <jonas.emdal@nordicsemi.no>
2021-10-06 12:21:03 +0200

    Use alert from shared

--- src/WarningView.tsx
+++ src/WarningView.tsx
@@ -4,35 +4,32 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
  */
 
-// eslint-disable-next-line import/no-unresolved
 import React from 'react';
-import Alert from 'react-bootstrap/Alert';
 import { useSelector } from 'react-redux';
+import { Alert } from 'pc-nrfconnect-shared';
 
 import {
     getCommunicationError,
-    getCompatibleDeviceWaring,
+    getCompatibleDeviceWarning,
 } from './reducers/warningReducer';
 
-const warningIcon = <span className="mdi mdi-sign warning-sign" />;
-
-const combineWarnings = (warnings: string[]) =>
-    warnings
-        .filter(str => str.length !== 0)
-        .map((s, index) => (
-            <Alert variant="danger" key={`warning-${index + 1}`}>
-                <span>{warningIcon}</span>
-                {s}
-            </Alert>
-        ));
-
 const WarningView = () => {
-    const compatibleDeviceWarning = useSelector(getCompatibleDeviceWaring);
+    const compatibleDeviceWarning = useSelector(getCompatibleDeviceWarning);
     const communicationError = useSelector(getCommunicationError);
+
     return (
-        <div className="warning-view">
-            {combineWarnings([compatibleDeviceWarning, communicationError])}
-        </div>
+        <>
+            {compatibleDeviceWarning && (
+                <Alert variant="warning" label="">
+                    {compatibleDeviceWarning}
+                </Alert>
+            )}
+            {communicationError && (
+                <Alert variant="danger" label="">
+                    {communicationError}
+                </Alert>
+            )}
+        </>
     );
 };
 

6aacf7dc4b07f55a5a00f2b4b5be2d4f13d164dc
Pascal Pickel <Pascal.Pickel@nordicsemi.no>
2022-04-29 15:37:43 +0200

    test: remove unused mock filter
    Signed-off-by: Pascal Pickel <Pascal.Pickel@nordicsemi.no>

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,4 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
  */
 
-module.exports = require('pc-nrfconnect-shared/config/jest.config')([
-    'serialport',
-]);
+module.exports = require('pc-nrfconnect-shared/config/jest.config')();

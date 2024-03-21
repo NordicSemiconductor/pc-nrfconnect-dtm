fa30c3c1edd33196017cafa54f411ee7a576b24f
kylebonnici <kylebonnici@hotmail.com>
2024-03-21 18:14:54 +0100

    Fix: reciving mapping

--- src/actions/testActions.ts
+++ src/actions/testActions.ts
@@ -222,7 +222,7 @@ export const startTests =
                     receivedChannels = new Array(40).fill(0);
 
                     if (received !== undefined) {
-                        receivedChannels[singleChannelIndexed] = received;
+                        receivedChannels[singleChannel] = received;
                     }
                 }
                 const testTypeStr =

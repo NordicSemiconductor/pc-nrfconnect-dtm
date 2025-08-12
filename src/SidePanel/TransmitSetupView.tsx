26d3c3832b9a67c3cf6ef53a9a82aebfe26d9dd7
Mia Pickel <Mia.Pickel@nordicsemi.no>
2025-08-12 10:20:58 +0200

    feat: remove tx filterings per device

--- src/SidePanel/TransmitSetupView.tsx
+++ src/SidePanel/TransmitSetupView.tsx
@@ -8,29 +8,25 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { NumberInput } from '@nordicsemiconductor/pc-nrfconnect-shared';
 
-import { getBoard } from '../reducers/deviceReducer';
 import { getTxPower, txPowerChanged } from '../reducers/settingsReducer';
 import { getIsRunning } from '../reducers/testReducer';
-import { fromPCA } from '../utils/boards';
+import dbmValues from '../utils/dbmValues';
 
 const TxPowerView = () => {
     const txPower = useSelector(getTxPower);
-    const boardType = useSelector(getBoard);
     const isRunning = useSelector(getIsRunning);
     const dispatch = useDispatch();
 
-    const dBmValues = fromPCA(boardType).txPower;
-
     useEffect(() => {
-        if (!dBmValues.includes(txPower)) {
+        if (!dbmValues.includes(txPower)) {
             // Currently the dBmValues will always contain a 0
             dispatch(
                 txPowerChanged(
-                    dBmValues.includes(0) ? 0 : dBmValues[dBmValues.length / 2]
+                    dbmValues.includes(0) ? 0 : dbmValues[dbmValues.length / 2]
                 )
             );
         }
-    }, [dispatch, dBmValues, txPower]);
+    }, [dispatch, txPower]);
 
     return (
         <NumberInput
@@ -39,7 +35,7 @@ const TxPowerView = () => {
             unit="dBm"
             label="Transmit power"
             value={txPower}
-            range={dBmValues}
+            range={dbmValues}
             disabled={isRunning}
             onChange={value => {
                 dispatch(txPowerChanged(value));

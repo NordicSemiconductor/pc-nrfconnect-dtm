c88249d96edef54878bc3088caa383c80b0a78b1
Mia Rosie Lleorenig <30871542+boundlesscalm@users.noreply.github.com>
2025-12-18 16:18:22 +0100

    feat: add tooltip to tx power slider (#246)
    * feat: add tooltip to tx power slider
    
    * Add changelog
    
    * Update Changelog.md
    
    Co-authored-by: Grzegorz Ferenc <41291385+greg-fer@users.noreply.github.com>
    
    * Latest edits
    
    ---------
    
    Co-authored-by: Sviatoslav <kiev.devel@gmail.com>
    Co-authored-by: Grzegorz Ferenc <41291385+greg-fer@users.noreply.github.com>

--- src/SidePanel/TransmitSetupView.tsx
+++ src/SidePanel/TransmitSetupView.tsx
@@ -6,7 +6,10 @@
 
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { NumberInput } from '@nordicsemiconductor/pc-nrfconnect-shared';
+import {
+    NumberInput,
+    Overlay,
+} from '@nordicsemiconductor/pc-nrfconnect-shared';
 
 import { getTxPower, txPowerChanged } from '../reducers/settingsReducer';
 import { getIsRunning } from '../reducers/testReducer';
@@ -22,7 +25,28 @@ const TxPowerView = () => {
             showSlider
             minWidth
             unit="dBm"
-            label="Transmit power"
+            label={
+                <Overlay
+                    tooltipId="tx-power-tooltip"
+                    tooltipChildren={
+                        <div className="tw-preflight tw-flex tw-flex-col tw-gap-4 tw-bg-gray-900 tw-px-4 tw-py-2 tw-text-left tw-text-gray-100">
+                            <p>
+                                Value of the SoC transmitter output power level,
+                                set by the 0x09 DTM command.
+                            </p>
+                            <p>
+                                When the selected output power level is not
+                                supported, the DTM command sets a power level
+                                that is nearest to the requested one, as per the
+                                Bluetooth®️ specification.
+                            </p>
+                        </div>
+                    }
+                >
+                    Transmit power{' '}
+                    <span className="mdi mdi-help-circle-outline tw-px-1" />
+                </Overlay>
+            }
             value={txPower}
             range={{
                 min: Math.min(...dbmValues),
